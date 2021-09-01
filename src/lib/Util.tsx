351703e74dfb72d73159cdd796575681898c8f7e
YasushiShimizu <decode4423@gmail.com>
2021-09-02 02:04:44 +0900

    [fix] 階層表示がおかしい，safariでicon読み込みできず落ちるバグ修正
    順番をpromiseで明示，loadingを一旦表示

--- src/lib/Util.tsx
+++ src/lib/Util.tsx
@@ -12,10 +12,42 @@ class Util{
         else if(Array.isArray(variable)){
             return variable != null ? variable : [];
         }
+        else if(variable instanceof HTMLImageElement){
+            if(variable.src != ""){
+                return variable;
+            }
+            const tmpImage = new window.Image();
+            tmpImage.src = 'img/top/loading.gif';
+            return tmpImage;
+        }
         return variable != null ? variable : false;
     }
 
-    public static getHTMLImage = (path: string, callback: (image: HTMLImageElement)=>void = ()=>{}) =>{
+    public static getHTMLImage = (path: string | string[], callback: (image: HTMLImageElement | HTMLImageElement[])=>void = ()=>{}) =>{
+        if(Array.isArray(path)){
+            let images: HTMLImageElement[] = [];
+
+            let loadImgae = (imagePath: string) => new Promise((resolve) => {
+                let image = new window.Image();
+                image.src = window.location.origin+"/img/"+imagePath;
+                image.onload = () => {
+                    // setState will redraw layer
+                    // because "image" property is changed
+                    images.push(image);
+                    resolve(null);
+                };
+            });
+
+            (async () => {
+                await path.reduce( (promise, p) => {
+                    return promise.then( async () => {
+                        await loadImgae(p);
+                    });
+                }, Promise.resolve());
+                callback(images);
+                return images;
+            })();
+        }
         let image = new window.Image();
         image.src = window.location.origin+"/img/"+path;
         image.onload = () => {
